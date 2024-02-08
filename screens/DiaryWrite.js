bb9c372ef5d1a75723da03b9d8a9c3117da4af2e
MY-yong <minyeong3@naver.com>
2024-02-09 00:43:06 +0900

    login 수정, date 추가

--- screens/DiaryWrite.js
+++ screens/DiaryWrite.js
@@ -68,11 +68,39 @@ const DiaryWrite = () => {
           source={require("../assets/padlock.png")}
         />
       </Pressable>
+
       <TextInput
         style={styles.diaryWriteItem}
         placeholder={`Enter Text >`}
         multiline={true}
       />
+
+<View style={[styles.groupContainer, styles.groupParentLayout]}>
+        <View style={styles.viewPosition}>
+          <View style={[styles.rectangleView, styles.frameChildBorder]} />
+          <TextInput
+            style={[styles.year, styles.yearTypo]}
+            placeholder="Year"
+            placeholderTextColor="#838282"
+          />
+        </View>
+        <View style={[styles.rectangleParent1, styles.groupParentLayout]}>
+          <View style={[styles.groupChild1, styles.frameChildBorder]} />
+          <TextInput
+            style={[styles.month, styles.yearTypo]}
+            placeholder="Month"
+            placeholderTextColor="#838282"
+          />
+        </View>
+        <View style={[styles.rectangleParent2, styles.groupParentLayout]}>
+          <View style={[styles.groupChild1, styles.frameChildBorder]} />
+          <TextInput
+            style={[styles.month, styles.yearTypo]}
+            placeholder="Date"
+            placeholderTextColor="#838282"
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -82,6 +110,400 @@ const styles = StyleSheet.create({
     width: 31,
     height: 30,
     position: "absolute",
+  },
+  groupContainer: {
+    top: 100,
+    width: 300,
+    left: 26,
+  },
+  groupParentLayout: {
+    height: 31,
+    position: "absolute",
+  },
+  rectangleParent1: {
+    left: 130,
+    top: 0,
+    width: 80,
+  },
+  rectangleParent2: {
+    left: 220,
+    top: 0,
+    width: 80,
+  },
+  frameScrollViewContent: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  parentLayout: {
+    height: 30,
+    position: "absolute",
+  },
+  sortLeftLayout: {
+    width: 30,
+    top: 0,
+    height: 30,
+    position: "absolute",
+  },
+  groupItemLayout: {
+    height: 300,
+    width: 300,
+    position: "absolute",
+  },
+  iHaveATypo: {
+    width: 252,
+    color: Color.colorDarkslategray_100,
+    textAlign: "left",
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+    position: "absolute",
+  },
+  groupLayout: {
+    height: 20,
+    width: 60,
+    position: "absolute",
+  },
+  editTypo: {
+    width: 37,
+    textAlign: "center",
+    color: Color.colorWhite,
+    fontSize: FontSize.size_4xs,
+    top: 2,
+    lineHeight: 15,
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+    position: "absolute",
+  },
+  groupParentLayout: {
+    height: 31,
+    position: "absolute",
+  },
+  frameChildBorder: {
+    borderWidth: 1,
+    borderColor: Color.colorGainsboro_200,
+    borderStyle: "solid",
+    backgroundColor: Color.colorWhite,
+  },
+  yearTypo: {
+    fontSize: FontSize.size_smi,
+    top: 7,
+    fontFamily: FontFamily.interRegular,
+    position: "absolute",
+  },
+  haveTypo: {
+    left: 16,
+    width: 126,
+    fontSize: FontSize.size_smi,
+    color: Color.colorDarkslategray_100,
+    textAlign: "left",
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+    position: "absolute",
+  },
+  iconLayout1: {
+    height: 74,
+    width: 93,
+    left: 144,
+    position: "absolute",
+  },
+  iconLayout: {
+    width: 44,
+    left: 243,
+    height: 74,
+    position: "absolute",
+  },
+  groupChild: {
+    left: 1,
+  },
+  icon: {
+    height: "100%",
+    width: "100%",
+  },
+  sortLeft: {
+    left: 0,
+  },
+  ellipseParent: {
+    width: 31,
+    left: 0,
+    top: 0,
+  },
+  diary: {
+    top: 6,
+    left: 41,
+    color: Color.colorBlack,
+    textAlign: "left",
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+    fontSize: FontSize.size_mini,
+    position: "absolute",
+  },
+  groupParent: {
+    top: 50,
+    left: 17,
+    width: 80,
+  },
+  groupItem: {
+    backgroundColor: Color.colorWhitesmoke_100,
+    borderRadius: Border.br_xl,
+    left: 0,
+    top: 0,
+  },
+  monday11March: {
+    top: 16,
+    color: Color.colorGray_300,
+    width: 165,
+    fontFamily: FontFamily.interRegular,
+    fontSize: FontSize.size_2xs,
+    left: 24,
+    textAlign: "left",
+    position: "absolute",
+  },
+  iHaveA: {
+    top: 36,
+    left: 24,
+    fontSize: FontSize.size_mini,
+    width: 252,
+    color: Color.colorDarkslategray_100,
+  },
+  iHaveA1: {
+    top: 201,
+    fontWeight: "300",
+    fontFamily: FontFamily.interLight,
+    lineHeight: 15,
+    width: 252,
+    color: Color.colorDarkslategray_100,
+    fontSize: FontSize.size_2xs,
+    left: 24,
+    textAlign: "left",
+    position: "absolute",
+  },
+  image49Icon: {
+    top: 91,
+    width: 150,
+    height: 100,
+    left: 24,
+    position: "absolute",
+  },
+  image50Icon: {
+    top: 89,
+    left: 184,
+    width: 70,
+    height: 100,
+    position: "absolute",
+  },
+  groupInner: {
+    backgroundColor: "#ff9900",
+    borderRadius: Border.br_3xs,
+    left: 0,
+    top: 0,
+  },
+  delete: {
+    left: 11,
+  },
+  rectangleGroup: {
+    left: 164,
+    top: 270,
+    width: 60,
+  },
+  rectanglePressable: {
+    borderRadius: Border.br_81xl,
+    backgroundColor: Color.colorGoldenrod_300,
+    left: 0,
+    top: 0,
+  },
+  edit: {
+    left: 12,
+  },
+  rectangleContainer: {
+    left: 230,
+    top: 270,
+    width: 60,
+  },
+  rectangleParent: {
+    top: 143,
+    left: 26,
+  },
+  rectangleView: {
+    width: 120,
+    height: 31,
+    left: 0,
+    top: 0,
+    position: "absolute",
+    borderRadius: Border.br_xl,
+  },
+  year: {
+    left: 35,
+  },
+  viewPosition: {
+    width: 120,
+    height: 31,
+    left: 0,
+    top: 0,
+    position: "absolute",
+  },
+  groupChild1: {
+    height: 31,
+    position: "absolute",
+    borderRadius: Border.br_xl,
+    left: 0,
+    top: 0,
+    width: 80,
+  },
+  month: {
+    left: 15,
+  },
+  rectangleParent1: {
+    left: 130,
+    top: 0,
+    width: 80,
+  },
+  rectangleParent2: {
+    left: 220,
+    top: 0,
+    width: 80,
+  },
+  groupContainer: {
+    top: 100,
+    width: 300,
+    left: 26,
+  },
+  otherMomsDiary: {
+    fontSize: FontSize.size_mid,
+    left: 0,
+    top: 0,
+  },
+  frameChild: {
+    zIndex: 0,
+    borderRadius: Border.br_3xs,
+    height: 100,
+    width: 300,
+  },
+  frameItem: {
+    zIndex: 1,
+    marginTop: 18,
+    borderWidth: 1,
+    borderColor: Color.colorGainsboro_200,
+    borderStyle: "solid",
+    borderRadius: Border.br_3xs,
+    height: 100,
+    width: 300,
+    backgroundColor: Color.colorWhite,
+  },
+  iHaveA2: {
+    zIndex: 2,
+    width: 126,
+    marginTop: 18,
+    fontSize: FontSize.size_smi,
+    color: Color.colorDarkslategray_100,
+    textAlign: "left",
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+  },
+  frameInner: {
+    top: 340,
+    zIndex: 3,
+    borderRadius: Border.br_3xs,
+    height: 100,
+    width: 300,
+    left: 0,
+    position: "absolute",
+  },
+  frameChild1: {
+    top: 450,
+    zIndex: 4,
+    borderRadius: Border.br_3xs,
+    height: 100,
+    width: 300,
+    left: 0,
+    position: "absolute",
+  },
+  iHaveA3: {
+    top: 355,
+    zIndex: 5,
+  },
+  image70Icon: {
+    top: 353,
+    zIndex: 6,
+  },
+  image71Icon: {
+    top: 352,
+    zIndex: 7,
+  },
+  iHaveA4: {
+    top: 465,
+    zIndex: 8,
+  },
+  image72Icon: {
+    top: 463,
+    zIndex: 9,
+  },
+  image73Icon: {
+    top: 462,
+    zIndex: 10,
+  },
+  frameChild2: {
+    top: 230,
+    zIndex: 11,
+    borderRadius: Border.br_3xs,
+    height: 100,
+    width: 300,
+    left: 0,
+    position: "absolute",
+  },
+  image68Icon: {
+    top: 243,
+    zIndex: 12,
+  },
+  image69Icon: {
+    top: 242,
+    zIndex: 13,
+  },
+  iHaveA5: {
+    top: 15,
+    zIndex: 14,
+  },
+  image53Icon: {
+    top: 13,
+    zIndex: 15,
+  },
+  image54Icon: {
+    top: 12,
+    zIndex: 16,
+  },
+  iHaveA6: {
+    top: 125,
+    zIndex: 17,
+  },
+  image66Icon: {
+    top: 123,
+    zIndex: 18,
+  },
+  image67Icon: {
+    top: 122,
+    zIndex: 19,
+  },
+  iHaveA7: {
+    top: 245,
+    zIndex: 20,
+  },
+  frameScrollview: {
+    top: 31,
+    left: 4,
+    position: "absolute",
+    flex: 1,
+  },
+  otherMomsDiaryParent: {
+    top: 470,
+    width: 304,
+    height: 318,
+    left: 26,
+    position: "absolute",
+  },
+  diaryCalender: {
+    height: 800,
+    width: "100%",
+    flex: 1,
+    backgroundColor: Color.colorWhite,
   },
   sortLeftPosition: {
     left: 0,
--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,76 +1,59 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View, Pressable, Linking } from "react-native"; // Linking 추가
+import { Image } from "expo-image";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { FontFamily, Color, FontSize, Border } from "../GlobalStyles";
 
 const LogIn = () => {
   const navigation = useNavigation();
 
-  // 버튼을 누르면 지정된 URL로 이동하는 함수
-  const handlePress = () => {
-    // 지정된 URL로 이동합니다.
-    Linking.openURL("https://applemango.store/oauth2/authorization/google");
-  };
-
   return (
     <View style={styles.logIn}>
       <Image
         style={styles.image70Icon}
         contentFit="cover"
-        source={require("../assets/image-701.png")}
+        source={require("../assets/image-69.png")}
       />
-      <View style={styles.groupParent}>
-        <View style={styles.letsPlayParent}>
-          <Text
-            style={[styles.letsPlay, styles.letsPlayTypo]}
-          >{`Let's play `}</Text>
-          <Image
-            style={styles.groupChild}
-            contentFit="cover"
-            source={require("../assets/line-21.png")}
-          />
-          <Image
-            style={styles.groupItem}
-            contentFit="cover"
-            source={require("../assets/line-23.png")}
-          />
-          <Image
-            style={styles.groupInner}
-            contentFit="cover"
-            source={require("../assets/line-22.png")}
-          />
-        </View>
-        <View style={[styles.customizedGamesWrapper, styles.customizedLayout]}>
-          <Text
-            style={[styles.customizedGames, styles.customizedLayout]}
-          >{`customized 
+      <Text
+        style={[styles.letsPlay, styles.letsPlayTypo]}
+      >{`Let's play `}</Text>
+      <Text style={[styles.forOurKids, styles.letsPlayTypo]}>
+        for our kids!
+      </Text>
+      <Text style={[styles.customizedGames, styles.letsPlayTypo]}>{`customized 
 games`}</Text>
-        </View>
-        <View style={[styles.forOurKidsParent, styles.forLayout]}>
-          <Text style={[styles.forOurKids, styles.forLayout]}>
-            for our kids!
-          </Text>
-          <Image
-            style={styles.vectorIcon}
-            contentFit="cover"
-            source={require("../assets/vector-1.png")}
-          />
-        </View>
-      </View>
+      <Image
+        style={styles.logInChild}
+        contentFit="cover"
+        source={require("../assets/vector-1.png")}
+      />
+      <Image
+        style={styles.logInItem}
+        contentFit="cover"
+        source={require("../assets/line-21.png")}
+      />
+      <Image
+        style={styles.logInInner}
+        contentFit="cover"
+        source={require("../assets/line-23.png")}
+      />
+      <Image
+        style={styles.lineIcon}
+        contentFit="cover"
+        source={require("../assets/line-22.png")}
+      />
       <Pressable
-        style={[styles.rectangleParent, styles.rectangleLayout]}
-        onPress={handlePress} // 버튼을 누르면 handlePress 함수 호출
-      >
-        <View style={[styles.rectangleView, styles.rectangleLayout]} />
-        <Image
-          style={styles.googleIcon}
-          contentFit="cover"
-          source={require("../assets/google.png")}
-        />
-        <Text style={[styles.continueWithGoogle, styles.letsPlayTypo]}>
-          Continue with Google
-        </Text>
-      </Pressable>
+        style={styles.rectanglePressable}
+        onPress={() => navigation.navigate("Main")}
+      />
+      <Image
+        style={styles.googleIcon}
+        contentFit="cover"
+        source={require("../assets/google.png")}
+      />
+      <Text style={[styles.continueWithGoogle, styles.letsPlayTypo]}>
+        Continue with Google
+      </Text>
     </View>
   );
 };
@@ -83,28 +66,15 @@ const styles = StyleSheet.create({
     lineHeight: 36,
     position: "absolute",
   },
-  customizedLayout: {
-    height: 94,
-    width: 280,
-  },
-  forLayout: {
-    height: 52,
-    width: 272,
-  },
-  rectangleLayout: {
-    height: 50,
-    width: 300,
-    position: "absolute",
-  },
   image70Icon: {
-    width: 841,
-    height: 1138,
-    zIndex: 0,
+    top: 0,
+    left: 0,
+    width: 362,
+    position: "absolute",
+    height: 800,
   },
   letsPlay: {
-    top: 19,
-    width: 227,
-    height: 56,
+    top: 70,
     color: Color.colorWhite,
     textAlign: "left",
     fontFamily: FontFamily.interSemiBold,
@@ -112,34 +82,21 @@ const styles = StyleSheet.create({
     lineHeight: 36,
     letterSpacing: -1.5,
     fontSize: FontSize.size_31xl,
-    left: 0,
+    left: 30,
   },
-  groupChild: {
-    left: 221,
-    width: 5,
-    height: 18,
-    top: 0,
-    position: "absolute",
-  },
-  groupItem: {
-    top: 29,
-    left: 236,
-    width: 15,
-    height: 6,
-    position: "absolute",
-  },
-  groupInner: {
-    top: 11,
-    left: 231,
-    width: 11,
-    height: 13,
-    position: "absolute",
-  },
-  letsPlayParent: {
-    width: 251,
-    height: 75,
+  forOurKids: {
+    top: 221,
+    color: Color.colorWhite,
+    textAlign: "left",
+    fontFamily: FontFamily.interSemiBold,
+    fontWeight: "600",
+    lineHeight: 36,
+    letterSpacing: -1.5,
+    fontSize: FontSize.size_31xl,
+    left: 30,
   },
   customizedGames: {
+    top: 126,
     color: Color.colorGoldenrod_200,
     textShadowColor: "rgba(255, 192, 69, 0.8)",
     textShadowOffset: {
@@ -147,66 +104,62 @@ const styles = StyleSheet.create({
       height: 0,
     },
     textShadowRadius: 30,
-    top: 0,
     textAlign: "left",
     fontFamily: FontFamily.interSemiBold,
     fontWeight: "600",
     lineHeight: 36,
-    position: "absolute",
     letterSpacing: -1.5,
     fontSize: FontSize.size_31xl,
-    width: 280,
-    left: 0,
+    left: 30,
   },
-  customizedGamesWrapper: {
-    marginTop: 20,
-  },
-  forOurKids: {
-    top: 0,
-    textAlign: "left",
-    fontFamily: FontFamily.interSemiBold,
-    fontWeight: "600",
-    lineHeight: 36,
-    position: "absolute",
-    color: Color.colorWhite,
-    letterSpacing: -1.5,
-    fontSize: FontSize.size_31xl,
-    left: 0,
-  },
-  vectorIcon: {
-    top: 39,
-    left: 159,
+  logInChild: {
+    top: 262,
+    left: 189,
     borderRadius: Border.br_3xs,
     width: 101,
-    height: 9,
+    height: 10,
     position: "absolute",
   },
-  forOurKidsParent: {
-    marginTop: 20,
-  },
-  groupParent: {
+  logInItem: {
     top: 55,
-    zIndex: 1,
+    left: 239,
+    width: 5,
+    height: 14,
+    position: "absolute",
+  },
+  logInInner: {
+    top: 78,
+    left: 253,
+    width: 14,
+    height: 5,
+    position: "absolute",
+  },
+  lineIcon: {
+    top: 64,
+    left: 248,
+    width: 11,
+    height: 11,
+    position: "absolute",
+  },
+  rectanglePressable: {
+    top: 680,
+    borderRadius: 50,
+    width: 300,
+    height: 50,
     left: 30,
     position: "absolute",
-  },
-  rectangleView: {
-    borderRadius: 50,
-    top: 0,
-    left: 0,
-    width: 300,
     backgroundColor: Color.colorWhite,
   },
   googleIcon: {
-    top: 10,
-    left: 50,
+    top: 690,
+    left: 80,
     width: 30,
     height: 30,
     position: "absolute",
   },
   continueWithGoogle: {
-    top: 7,
-    left: 101,
+    top: 687,
+    left: 131,
     fontSize: FontSize.size_smi,
     color: "#0a0a0a",
     textAlign: "left",
@@ -214,17 +167,11 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     lineHeight: 36,
   },
-  rectangleParent: {
-    top: 680,
-    zIndex: 2,
-    left: 30,
-  },
   logIn: {
     flex: 1,
     width: "100%",
-    height: 800,
     overflow: "hidden",
-    flexDirection: "row",
+    height: 800,
     backgroundColor: Color.colorWhite,
   },
 });
